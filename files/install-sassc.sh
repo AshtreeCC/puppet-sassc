62214eed483494a06c5373922aa732e8a4cac644
Andrew Nash <akahadaka@gmail.com>
2014-05-30 13:59:43 +0200

    Updated submodule init for libsass

--- files/install-sassc.sh
+++ files/install-sassc.sh
@@ -7,7 +7,8 @@ cd /home/vagrant
 git clone git://github.com/hcatlin/sassc.git
 git clone git://github.com/hcatlin/libsass.git
 export SASS_LIBSASS_PATH=/home/vagrant/libsass
-cd sassc/
+cd libsass/
 git submodule init
 git submodule update
+cd ../sassc/
 make
